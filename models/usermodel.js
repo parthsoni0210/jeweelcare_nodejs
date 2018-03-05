08429724fce36d61307a43212aabbeeefe66bb1e
keyuryo <rathodkeyur42@gmail.com>
2018-03-05 13:26:00 +0530

    jewelcare services

--- models/usermodel.js
+++ models/usermodel.js
@@ -7,7 +7,7 @@ var users={
         return  db.query('select * from user_table where user_email=? and user_password=?',[user.user_email,user.user_password],callback);
         },
         sign_up:function(user,filename,callback){
-            filename="http://localhost:3000/images/users/"+filename;
+            filename="https://jewelcare.herokuapp.com/images/users/"+filename;
             return db.query('insert into user_table values(?,?,?,?,?,?,?,?)',[user.user_name,user.user_mobile_no,user.user_email,user.user_gender,user.user_bday,user.user_address,filename,user.user_password],callback);
         },
         getalluser:function(callback){           
@@ -17,7 +17,7 @@ var users={
             return db.query('select * from user_table where user_email=?',[id],callback);
         },
         editUser:function(id,user,filename,callback){
-            filename="http://localhost:3000/images/users/"+filename;
+            filename="https://jewelcare.herokuapp.com/images/users/"+filename;
             return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,filename,id],callback)
         },
         changePass(user,callback){
