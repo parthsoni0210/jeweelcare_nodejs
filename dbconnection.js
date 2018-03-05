226ef4568683fbf6943eb6d182fc8f8c49399edb
keyuryo <rathodkeyur42@gmail.com>
2018-03-05 10:36:42 +0530

    jewelcare

--- dbconnection.js
+++ dbconnection.js
@@ -1,7 +1,7 @@
 var mysql=require('mysql');
 var cnn=mysql.createPool({
 
-host:'jewelcare.db.9462939.19c.hostedresource.net ',
+host:'jewelcare.db.9462939.19c.hostedresource.net',
 user:'jewelcare',
 password:'Keyur@9898',
 database:'jewelcare' 
