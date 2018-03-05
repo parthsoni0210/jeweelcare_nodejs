67fe2409e7ab4feacc237c6eae558665955793e3
keyuryo <rathodkeyur42@gmail.com>
2018-03-05 22:07:23 +0530

    jewelcare

--- models/usermodel.js
+++ models/usermodel.js
@@ -16,9 +16,12 @@ var users={
         getUserById:function(id,callback){
             return db.query('select * from user_table where user_email=?',[id],callback);
         },
-        editUser:function(id,user,filename,callback){
+        editUserImgUpload:function(id,user,filename,callback){
             filename="https://jewelcare.herokuapp.com/images/users/"+filename;
             return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,filename,id],callback)
+        },
+        editUser:function(id,user,callback){
+            return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,user.user_image,id],callback)
         },
         changePass(user,callback){
             return  db.query('update user_table set user_password=? where user_email=?',[user.user_password,user.user_email],callback);
--- routes/user_route.js
+++ routes/user_route.js
@@ -3,44 +3,56 @@ var router = express.Router();
 
 var multer = require('multer');
 var path = require('path');
-var user=require('../models/usermodel');
+var user = require('../models/usermodel');
 
-router.get('/:id',function(req,res,next){
+router.get('/:id', function (req, res, next) {
 
-        user.getUserById(req.params.id,function(err,rows){
+    user.getUserById(req.params.id, function (err, rows) {
 
-            if(err){
-                res.json(err);
-            }
-            else{
-                res.json(rows);
-            }
-        });
-});
-
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/images/users')
-    },
-    filename: (req, file, cb) => {
-      x=file.fieldname + '-' + Date.now()+path.extname(file.originalname);
-      cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname))
-    }
-});
-var upload = multer({storage: storage});
-
-router.put('/:id',upload.single('image'),function(req,res,next){
-
-    user.editUser(req.params.id,req.body,req.file.filename,function(err,rows){
-
-        if(err){
+        if (err) {
             res.json(err);
-        }
-        else{
+        } else {
             res.json(rows);
         }
     });
 });
 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images/users')
+    },
+    filename: (req, file, cb) => {
+        x = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+var upload = multer({
+    storage: storage
+});
 
-module.exports=router;
+router.put('/:id/:flag?', upload.single('image'), function (req, res, next) {
+    if (req.params.flag) {
+        user.editUserImgUpload(req.params.id, req.body, req.file.filename, function (err, rows) {
+
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+    else
+    {
+        user.editUser(req.params.id, req.body, function (err, rows) {
+
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+});
+
+
+module.exports = router;
