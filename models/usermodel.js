ee49e970f1e7bf60836db86b888b89004ccb6449
keyuryo <rathodkeyur42@gmail.com>
2018-03-07 10:59:59 +0530

    jewelcare

--- models/usermodel.js
+++ models/usermodel.js
@@ -17,7 +17,7 @@ var users={
             return db.query('select * from user_table where user_email=?',[id],callback);
         },
         editUserImgUpload:function(user,filename,callback){
-            filename="https://jewelcare.herokuapp.com/images/users/"+filename;
+            //filename="https://jewelcare.herokuapp.com/images/users/"+filename;
             return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,filename,user.user_email],callback)
         },
         editUser:function(id,user,callback){
