fa47ab9770e955b8d4e1bea9ff5c2cd7dcc09070
keyuryo <rathodkeyur42@gmail.com>
2018-03-20 11:10:42 +0530

    jewel

--- models/CountModel.js
+++ models/CountModel.js
@@ -3,16 +3,16 @@ var db = require('../dbconnection'); //reference of dbconnection.js
 var cnt = {
 
     getcntUser(callback) {
-        return db.query("select COUNT(*) from user_table");
+        return db.query("select COUNT(*) from user_table",callback);
     },
     getcntProduct(callback) {
-        return db.query("select COUNT(*) from product_table");
+        return db.query("select COUNT(*) from product_table",callback);
     },
     getcntOrder(callback) {
-        return db.query("select COUNT(*) from order_table");
+        return db.query("select COUNT(*) from order_table",callback);
     },
     getcntEmp(callback) {
-        return db.query("select COUNT(*) from org_user_table");
+        return db.query("select COUNT(*) from org_user_table",callback);
     }
 
 }
