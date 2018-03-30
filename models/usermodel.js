840fae399698062631971b40e8943129b1ec332a
keyuryo <rathodkeyur42@gmail.com>
2018-03-30 17:16:08 +0530

    edit user

--- models/usermodel.js
+++ models/usermodel.js
@@ -23,10 +23,10 @@ var users={
         },
         editUserImgUpload:function(user,filename,callback){
             filename="https://jewelcare.herokuapp.com/images/"+filename;
-            return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,filename,user.user_email],callback)
+            return db.query('update user_table set user_name=?,user_address=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_address,user.user_mobile_no,user.user_gender,user.user_bday,filename,user.user_email],callback)
         },
         editUser:function(user,callback){
-            return db.query('update user_table set user_name=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_mobile_no,user.user_gender,user.user_bday,user.user_image,user.user_email],callback)
+            return db.query('update user_table set user_name=?,user_address=?, user_mobile_no=?, user_gender=?, user_bday=?, user_image=? where user_email=?',[user.user_name,user.user_address,user.user_mobile_no,user.user_gender,user.user_bday,user.user_image,user.user_email],callback)
         },
         changePass(user,callback){
             return  db.query('update user_table set user_password=? where user_email=?',[user.user_password,user.user_email],callback);
