0541ecdc39ab39c97ddfef813abb2bb12460f212
keyuryo <rathodkeyur42@gmail.com>
2018-04-28 09:59:35 +0530

    forget password

--- models/emailModel.js
+++ models/emailModel.js
@@ -1,24 +1,24 @@
-var email = require("emailjs/email");
-var forgetPass = {
+var email   = require("emailjs/email");
+var demo={
 
-    sendMail: function (demo, callback) {
+sendMail:function(demo,callback){
 
-        var server = email.server.connect({
-            user: "parth.soni0210@gmail.com",
-            password: "parth#@123",
-            host: "smtp.gmail.com",
-            ssl: true,
-            port: 465
-        });
+var server  = email.server.connect({
+   user:    "parth.soni0210@gmail.com", 
+   password:"parth#@123", 
+   host:    "smtp.gmail.com", 
+   ssl:     true,
+   port:465
+});
 
-        server.send({
-            text:    demo.message,
-            from:    "reunirc@gmail.com", 
-            to:      demo.name,
-            subject: demo.subject
-        }, callback);
-    }
+server.send({
+   text:    demo.message,
+   from:    "reunirc@gmail.com", 
+   to:      demo.name,
+   subject: demo.subject
+}, callback);
+}
 
 
 }
-module.exports = forgetPass;
+module.exports = demo;
