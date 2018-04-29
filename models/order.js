d47e74d91fab20f1c65014b187b062a899870036
keyuryo <rathodkeyur42@gmail.com>
2018-04-29 16:38:58 +0530

    email

--- models/order.js
+++ models/order.js
@@ -45,7 +45,7 @@ var order={
     
     orderChart:function(branch,cat,callback)
     {
-        db.query("select o.*,c.* from order_table o,category_table c,product_table p where o.delivery_address like '%"+branch+"%' and o.fk_product_id = p.product_id and p.fk_cat_id = c.cat_id and c.cat_name = ?",[cat],callback);
+        db.query("select o.*,sum(o.product_qty) from order_table o,category_table c,product_table p where o.delivery_address like '%"+branch+"%' and o.fk_product_id = p.product_id and p.fk_cat_id = c.cat_id and c.cat_name = ?",[cat],callback);
     }
 
 };
